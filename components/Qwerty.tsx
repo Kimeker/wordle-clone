cf93ce8e8d4cc246a521688ccc7ac416852b8e61
Kimeker <kimkarleker@gmail.com>
2022-12-06 14:46:48 +0100

    first commit

---
+++ components/Qwerty.tsx
@@ -0,0 +1,29 @@
+import { observer } from 'mobx-react-lite'
+
+export default observer(function Querty({ store }) {
+  const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+  return (
+    <div className='m-10'>
+      {qwerty.map((row) => (
+        <div className="flex justify-center p-1 gap-1">
+          {row.split('').map((char) => {
+            const bgColor = store.exactGuesses.includes(char)
+              ? 'bg-green-400'
+              : store.inexactGuesses.includes(char)
+              ? 'bg-yellow-400'
+              : store.allGuesses.includes(char)
+              ? 'bg-gray-400'
+              : 'bg-gray-200'
+            return (
+              <div
+                className={`rounded-m m-1 flex h-10 w-10 items-center justify-center uppercase ${bgColor}`}
+              >
+                {char}
+              </div>
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
+})
